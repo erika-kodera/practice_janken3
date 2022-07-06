9f47261e353c078e684e5614ba87314db8c668d7
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-07-06 17:20:12 +0900

    人間に手を入力してもらう機能
    人間に手を入力してもらう機能を関数定義しました。

--- js/app.js
+++ js/app.js
@@ -3,3 +3,15 @@
 const GU    = 1;
 const CHOKI = 2;
 const GU    = 3;
+
+/* 関数定義 ***********/
+// 人間に手を入力してもらう機能
+function getHumHand() {
+  let hum = prompt('半角数字で1〜3の数字を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー');
+  hum = parseInt(hum, 10);
+  if (hum !== GU && hum !== CHOKI && hum !== PA) {
+    return undefined;
+  } else {
+    return hum;
+  }
+}
